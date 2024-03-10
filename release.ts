0eaaafd368a0fce59a4c2dbf53d856441572f1fe
Stefan Heule <stefanheule@gmail.com>
2024-03-09 16:55:37 -0800

    work in progress

--- release.ts
+++ release.ts
@@ -16,7 +16,11 @@ async function run(command: string) {
 
 async function main() {
   await run(`yarn build`);
-  for (const project of ['metro', 'purplemoon']) {
+
+  // No need to do anything on terra other than update the repo and rebuild. Deploying projects does a fresh install of dependencies, so that's fine.
+  await run(`ssh terra NO_GITSTATUS=yes 'zsh -ic "cd ~/www/check-type && git pull && yarn install && yarn build"'`);
+
+  for (const project of ['metro', 'purplemoon', 'call-schedule']) {
     await run(`rm -f ${HERE}/../${project}/node_modules/.bin/generate-schema`);
     for (const dir of ['', 'server/', 'client/']) {
       await run(`rm -rf ${HERE}/../${project}/${dir}node_modules/check-type`);
---
+++ update.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# IMPORTANT: This script is not used, use yarn release instead
+
+set -e
+
+prefix="\e[1;34m[update.sh] "
+suffix="\e[0m"
+
+echo "${prefix}Building check-type locally...${suffix}"
+yarn build
+
+# update check-type on terra via ssh
+echo -e "${prefix}Updating check-type on terra...${suffix}"
+ssh terra NO_GITSTATUS=yes 'zsh -ic "cd ~/www/check-type && git pull && yarn install && yarn build"'
+
+# run build in ../metro and ../purplemoon
+for d in metro purplemoon call-schedule; do
+  dir="../$d"
+  if [ -d "$dir" ]; then
+    echo "${prefix}Updating dependency for $d...${suffix}"
+    (cd "$dir" && rm -rf node_modules client/node_modules server/node_modules && yarn install-all && yarn codegen)
+    echo "${prefix}Updating dependency for $d on terra...${suffix}"
+    ssh terra NO_GITSTATUS=yes "zsh -ic \"cd ~/www/$d-staging && rm -rf node_modules client/node_modules server/node_modules && yarn install-all\""
+  else
+    echo "${prefix}Project $d does not exist!!${suffix}"
+  fi
+done
+
+echo "${prefix}Done${suffix}"
